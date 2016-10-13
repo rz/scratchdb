1c921f8b1aa80d3e407bce4ca77fbca9ab46c8e9
Rodrigo Guzman <rodguze@gmail.com>
2016-10-13 07:51:03 -0400

    set up a class for each component

---
+++ scratchdb.py
@@ -0,0 +1,25 @@
+### A simple key-value database that supports set, get, and pop operations.
+
+
+# The physical layer
+class FileStorage(object):
+    pass
+
+
+# The logical layer
+class Logical(object):
+    pass
+
+
+# The database API
+class ScratchDB(object):
+    pass
+
+
+class QueryProcessor(object):
+    pass
+
+
+class Client(object):
+    pass
+
