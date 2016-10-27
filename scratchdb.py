223195e5d87df476755ab1b9848edce225e55296
Rodrigo Guzman <rodguze@gmail.com>
2016-10-26 21:23:00 -0400

    add utility methods to FileStorage

--- scratchdb.py
+++ scratchdb.py
@@ -1,6 +1,7 @@
 ### A simple key-value database that supports set, get, and pop operations.
 
 import os
+import struct
 
 
 # The physical layer
@@ -21,10 +22,14 @@ class FileStorage(object):
         # ensure there are zero bytes at the end of the file
         self._zero_end()
 
-    # internal methods
+    # methods that interact with the file itself ie the wrapper around it
     def _seek(self, offset, whence=0):
         """Wrapper around File.seek() for consistency."""
         return self._f.seek(offset, whence)
+
+    def _seek_start(self):
+        """Moves the stream position to the beginning of the file and returns that address."""
+        return self._f.seek(0, os.SEEK_SET)
 
     def _seek_end(self):
         """Moves the stream position to the end of file and returns that address."""
@@ -44,6 +49,7 @@ class FileStorage(object):
         self._f.flush()
         return addr
 
+    # internal utility methods
     def _zero_end(self):
         """
         Writes zero bytes at the end of the file so that reading a 0 integer at
@@ -51,13 +57,23 @@ class FileStorage(object):
         """
         if self._is_empty() or self._seek_end() < self.INTEGER_LENGTH:
             self._seek_end()
-            self._write(b'\x00' * self.INTEGER_LENGTH)
+            self._write_integer(0)
         else:
             self._seek(-self.INTEGER_LENGTH, os.SEEK_END)
             last_bytes = self._read(self.INTEGER_LENGTH)
             if any([byte != b'\x00' for byte in last_bytes]):
                 self._seek_end()
-                self._write(b'\x00' * self.INTEGER_LENGTH)
+                self._write_integer(0)
+
+    def _read_integer(self):
+        """Reads an integer from the file at the current stream position."""
+        bs = self._read(self.INTEGER_LENGTH)
+        return struct.unpack(self.INTEGER_FORMAT, bs)[0]  # struct.unpack always returns a tuple
+
+    def _write_integer(self, n):
+        """Writes an integer to the file at the current stream position."""
+        bs = struct.pack(self.INTEGER_FORMAT, n)
+        self._write(bs)
 
     # the external api
     def read(self, address):
