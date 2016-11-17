3898016a385074107bbaac7a6137ac47b0921359
Rodrigo Guzman <rodguze@gmail.com>
2016-11-17 15:06:48 -0500

    add a show() method to LogicalLinkedList and a driver for sanity-checking

--- ll.py
+++ ll.py
@@ -95,3 +95,43 @@ class LogicalLinkedList(object):
     def pop(self, key):
         pass
 
+    def show(self):
+        result_str = ''
+        node, _ = self._get_head()
+        while node is not None:
+            if node.value_address is None:
+                value = None
+            else:
+                value = self._storage.read(node.value_address)
+            result_str += '[%s value_addr:%s]-->' % (node.key, node.value_address)
+            if node.next_address is None:
+                node = None
+            else:
+                node = self._storage.read(node.next_address)
+        print('list: %s' % result_str)
+        print('head address: %s' % self._storage.get_head_address())
+        print('storage:')
+        self._storage.show()
+        print('-'*30)
+
+
+if __name__ == '__main__':
+    ll = LogicalLinkedList()
+    print('inserting 5 keys: k0,k1,k2,k3,k4')
+    for i in range(5):
+        ll.set('k%s' %i, 'v%s' % i)
+        ll.show()
+    for i in range(5):
+        print('get k%s:' % i, ll.get('k%s' % i))
+    print('getting non-existent:', end=' ')
+    try:
+        ll.get('DNE')
+    except KeyError as e:
+        print('got KeyError', e, end='\n\n')
+
+    print('updating k2 to new v2')
+    ll.set('k2', 'new v2')
+    ll.show()
+
+    print('get k2:', ll.get('k2'))
+
